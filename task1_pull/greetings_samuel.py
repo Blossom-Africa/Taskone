384cfbe67fd26e7842edd5d6353cb1d823a59fc3
Blossom Africa <courses@blossom.africa>
2026-06-24 11:39:10 +0100

    Add greetings and farewell functions

---
+++ task1_pull/greetings_samuel.py
@@ -0,0 +1,11 @@
+
+def greet(name):
+    return f"Hello {name}, welcome to blosom academy"
+
+def farewell(name):
+    return f"Goodbye {name}, see you next class"
+
+
+if __name__ == "__main__":
+    print(greet("Student"))
+    print(farewell("Student"))
